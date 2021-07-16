6eac8154fff1600fea0b6ab6c78e304d3c47f3d7
vishaloza <vishoza@gmail.com>
2021-07-16 14:23:21 -0500

    CRAN submission

--- R/data.R
+++ R/data.R
@@ -2,15 +2,15 @@
 #' Data Release 25.0
 #' GDC Product: Data
 #' Release Date: July 22, 2020
-#' Masked Copy Number variation data for Breast Cancer selected randomly from TCGA
+#' Masked Copy Number variation data for Breast Cancer for 10 unique samples selected randomly from TCGA
 #' @docType data
-#' @usage data(maskCNV.BRCA)
+#' @usage data(maskCNV_BRCA)
 #' @format An object of class dataframe
 #' @keywords dataset
 #' @references Koboldt, D., Fulton, R., McLellan, M. et al. (2012) Nature 490, 61–70
 #' \url{https://www.nature.com/articles/nature11412}
 #' @source \url{https://portal.gdc.cancer.gov/}
 #' @examples
-#' data(maskCNV.BRCA)
-#' \donttest{tai <- tai(maskCNV.BRCA)}
-"maskCNV.BRCA"
+#' data(maskCNV_BRCA)
+#' \donttest{tai <- tai(maskCNV_BRCA)}
+"maskCNV_BRCA"
---
+++ cran-comments.md
@@ -0,0 +1,9 @@
+## Test environments
+* local OS X install(x86_64, darwin17.0), R 4.0.5
+* win-builder (release)
+
+## R CMD check results
+There were no ERRORs, WARNINGs or NOTEs. 
+
+## Downstream dependencies
+There are currently no downstream dependencies for this package
